4867a74952967bfa7b46f9a7d4d1ffa42d157261
Christopher Seaman <86775+christopherseaman@users.noreply.github.com>
2025-04-08 11:29:28 -0700

    ready-ish

--- combine.sh
+++
@@ -1,8 +0,0 @@
-cat lectures/02/parts/jupyter.md \
-lectures/02/parts/debugging/overview.md \
-lectures/02/parts/debugging/tools.md \
-lectures/02/parts/debugging/simplifying.md \
-lectures/02/parts/debugging/dependencies.md \
-lectures/02/parts/debugging/workflows.md \
-lectures/02/parts/debugging/examples.md \
-lectures/02/parts/bigdata/notes.md > lectures/02/full_lecture.md
